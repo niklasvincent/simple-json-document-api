b18c3488a75919551653ceaffcc9c3e35947e191
Niklas Lindblad <niklas.lindblad@guardian.co.uk>
2015-11-17 13:24:17 +0000

    Listen on all interfaces

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.post('/content/:identifier', function (req, res) {
 	}
 });
 
-var server = app.listen(10000, function () {
+var server = app.listen(10000, '0.0.0.0', function () {
 
   var host = server.address().address;
   var port = server.address().port;
